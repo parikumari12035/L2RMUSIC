f5022a23a6a3aea53d776e4cbde85c6ea9b73da8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2025-01-03 13:26:59 +0530

    Update autoleave.py

--- L2RMUSIC/plugins/misc/autoleave.py
+++ L2RMUSIC/plugins/misc/autoleave.py
@@ -1,42 +1,109 @@
+#
+# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+#
+# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 import asyncio
-from datetime import datetime
 
 from pyrogram.enums import ChatType
 
 import config
 from L2RMUSIC import app
-from L2RMUSIC.core.call import Ashish, autoend
-from L2RMUSIC.utils.database import get_client, is_active_chat, is_autoend
+from L2RMUSIC.core.call import Ashish
+from L2RMUSIC.core.call import _st_ as clean
+from L2RMUSIC.utils.database import (
+    get_active_chats,
+    get_assistant,
+    get_client,
+    is_active_chat,
+    is_autoend,
+)
+
+LEAVE = True
+async def auto_leave():
+    if LEAVE == True:
+        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
+            from VIPMUSIC.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.get_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
+                        ]:
+                            chat_id = i.chat.id
+                            if chat_id not in [
+                                config.LOG_GROUP_ID,
+                                -1002159045835,
+                                -1002146211959,
+                            ]:
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(chat_id):
+                                    try:
+                                        await client.leave_chat(chat_id)
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
 
 
-
+asyncio.create_task(auto_leave())
 
 
 async def auto_end():
-    while not await asyncio.sleep(5):
-        ender = await is_autoend()
-        if not ender:
+    while not await asyncio.sleep(30):
+        if not await is_autoend():
             continue
-        for chat_id in autoend:
-            timer = autoend.get(chat_id)
-            if not timer:
-                continue
-            if datetime.now() > timer:
+
+        served_chats = await get_active_chats()
+
+        for chat_id in served_chats:
+            try:
                 if not await is_active_chat(chat_id):
-                    autoend[chat_id] = {}
+                    await clean(chat_id)
                     continue
-                autoend[chat_id] = {}
-                try:
-                    await Ashish.stop_stream(chat_id)
-                except:
-                    continue
-                try:
-                    await app.send_message(
+
+                userbot = await get_assistant(chat_id)
+                call_participants_id = [
+                    member.chat.id async for member in userbot.get_call_members(chat_id)
+                ]
+
+                if len(call_participants_id) <= 1:
+                    ok = await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "» Nᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ ᴛᴏ sᴏɴɢ ɪɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ.\n"
+                        "ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏᴛʜᴇʀᴡɪsᴇ ʙᴏᴛ ᴡɪʟʟ ᴇɴᴅ sᴏɴɢ ɪɴ 15 sᴇᴄᴏɴᴅs.",
                     )
-                except:
-                    continue
+                    await asyncio.sleep(15)
+
+                    call_participants_id = [
+                        member.chat.id
+                        async for member in userbot.get_call_members(chat_id)
+                    ]
+
+                    if len(call_participants_id) <= 1:
+                        await ok.delete()
+                        await VIP.stop_stream(chat_id)
+                        await app.send_message(
+                            chat_id,
+                            "» Nᴏ ᴏɴᴇ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ, sᴏ ᴛʜᴇ sᴏɴɢ ɪs ᴇɴᴅɪɴɢ ᴅᴜᴇ ᴛᴏ ɪɴᴀᴄᴛɪᴠɪᴛʏ.",
+                        )
+                        await clean(chat_id)
+            except:
+                continue
 
 
+# Start the auto_end task
 asyncio.create_task(auto_end())
