c3814223570ab989c0a2b4e71b2bb1c73a18b34a
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2025-01-09 14:00:28 +0530

    Update start.py

--- L2RMUSIC/utils/inline/start.py
+++ L2RMUSIC/utils/inline/start.py
@@ -16,6 +16,24 @@ def start_panel(_):
     return buttons
 
 
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from L2RMUSIC import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_11"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
+        ],
+    ]
+    return buttons
+
+
 def private_panel(_):
     buttons = [
         [
@@ -26,8 +44,9 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
+            
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_helper"
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
